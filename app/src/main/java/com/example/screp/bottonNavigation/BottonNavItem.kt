a8974de6992416ff154678f8151b893549273566
Xiaoming Ma <73438688+myxmxm@users.noreply.github.com>
2022-09-30 11:46:15 +0300

    Create BottonNavItem.kt

---
+++ app/src/main/java/com/example/screp/bottonNavigation/BottonNavItem.kt
@@ -0,0 +1,11 @@
+package com.example.screp.bottonNavigation
+
+import com.example.screp.R
+
+sealed class BottomNavItem(var title:String, var icon:Int, var screen_route:String){
+
+    object Record : BottomNavItem("Record", R.drawable.ic_record,"record")
+    object Graph: BottomNavItem("Graph",R.drawable.ic_graph,"graph")
+    object Weather: BottomNavItem("Weather",R.drawable.ic_weather,"weather")
+    object Photos: BottomNavItem("Photos",R.drawable.ic_photos,"photos")
+}
