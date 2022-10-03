252f04be030352cd68817635234effc6586390f7
GiangNguyen1207 <nguyengiang.nchg@gmail.com>
2022-10-03 20:23:42 +0300

    change applicationId in build.gradle file

--- app/src/main/java/com/example/screp/viewModels/WeatherViewModel.kt
+++ app/src/main/java/com/example/screp/viewModels/WeatherViewModel.kt
@@ -1,6 +1,5 @@
 package com.example.screp.viewModels
 
-import android.util.Log
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
