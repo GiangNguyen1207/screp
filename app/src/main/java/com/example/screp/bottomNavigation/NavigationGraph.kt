4eb09391913e801e5e465f3d0af32a0844d2e45e
Xiaoming Ma <73438688+myxmxm@users.noreply.github.com>
2022-10-05 13:58:24 +0300

    Update NavigationGraph.kt

--- app/src/main/java/com/example/screp/bottomNavigation/NavigationGraph.kt
+++ app/src/main/java/com/example/screp/bottomNavigation/NavigationGraph.kt
@@ -1,29 +1,67 @@
 package com.example.screp.bottomNavigation
 
+import android.os.Build
+import androidx.annotation.RequiresApi
 import androidx.compose.runtime.Composable
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
-import com.example.screp.screens.MapViewScreen
-import com.example.screp.screens.PhotosScreen
-import com.example.screp.screens.RecordStepCountScreen
-import com.example.screp.screens.WeatherScreen
+import com.example.screp.screens.*
+import com.example.screp.viewModels.PhotoAndMapViewModel
 import com.example.screp.viewModels.StepCountViewModel
+import com.google.android.gms.location.FusedLocationProviderClient
+import java.io.File
 
+@RequiresApi(Build.VERSION_CODES.N)
 @Composable
-fun NavigationGraph(navController: NavHostController, stepCountViewModel: StepCountViewModel) {
+fun NavigationGraph(
+    navController: NavHostController,
+    stepCountViewModel: StepCountViewModel,
+    photoAndMapViewModel: PhotoAndMapViewModel,
+    imgPath: File?,
+    fusedLocationProviderClient: FusedLocationProviderClient
+) {
     NavHost(navController, startDestination = BottomNavItem.Record.screen_route) {
         composable(BottomNavItem.Record.screen_route) {
-            MapViewScreen()
+            MapViewScreen(
+                navController = navController,
+                photoAndMapViewModel = photoAndMapViewModel,
+                fusedLocationProviderClient = fusedLocationProviderClient
+            )
         }
         composable(BottomNavItem.Graph.screen_route) {
-            RecordStepCountScreen(stepCountViewModel = stepCountViewModel )
+            RecordStepCountScreen(
+                stepCountViewModel = stepCountViewModel
+            )
         }
         composable(BottomNavItem.Weather.screen_route) {
             WeatherScreen()
         }
         composable(BottomNavItem.Photos.screen_route) {
-            PhotosScreen()
+            PhotosScreen(
+                photoAndMapViewModel = photoAndMapViewModel,
+                imgPath = imgPath,
+                fusedLocationProviderClient = fusedLocationProviderClient,
+                navController = navController
+            )
+        }
+        composable(BottomNavItem.PhotoDetail.screen_route + "/{photoName}") {
+            val photoName = it.arguments?.getString("photoName")
+            PhotoDetailScreen(
+                navController = navController,
+                photoName = photoName,
+                photoAndMapViewModel = photoAndMapViewModel
+            )
+        }
+        composable(BottomNavItem.Setting.screen_route) {
+            SettingScreen(
+                navController = navController
+            )
+        }
+        composable(BottomNavItem.SettingEdit.screen_route) {
+            SettingEditScreen(
+                navController = navController
+            )
         }
     }
 }
