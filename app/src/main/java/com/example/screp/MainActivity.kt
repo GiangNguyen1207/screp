b1cb77051f82cf0b2a46adc82d52baebd095bb63
Xiaoming Ma <73438688+myxmxm@users.noreply.github.com>
2022-10-05 13:54:33 +0300

    add PhotoAndMapViewModel, FusedLocationProviderClient variables and runtime permission request

--- app/src/main/java/com/example/screp/MainActivity.kt
+++ app/src/main/java/com/example/screp/MainActivity.kt
@@ -1,28 +1,45 @@
 package com.example.screp
 
+import android.Manifest
+import android.content.pm.PackageManager
+import android.os.Build
 import android.os.Bundle
+import android.os.Environment
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.annotation.RequiresApi
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Scaffold
 import androidx.compose.material.Surface
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.navigation.compose.rememberNavController
 import com.example.screp.bottomNavigation.BottomNavigation
 import com.example.screp.bottomNavigation.NavigationGraph
 import com.example.screp.ui.theme.ScrepTheme
+import com.example.screp.viewModels.PhotoAndMapViewModel
 import com.example.screp.viewModels.StepCountViewModel
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
 
 class MainActivity : ComponentActivity() {
     companion object {
         private lateinit var stepCountViewModel: StepCountViewModel
+        private lateinit var photoAndMapViewModel: PhotoAndMapViewModel
     }
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
+    private lateinit var fusedLocationProviderClient: FusedLocationProviderClient
 
+    @RequiresApi(Build.VERSION_CODES.N)
+    override fun onCreate(savedInstanceState: Bundle?) {
+        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this)
+        hasPermissions()
+        super.onCreate(savedInstanceState)
         stepCountViewModel = StepCountViewModel(application)
+        photoAndMapViewModel = PhotoAndMapViewModel(application)
+        val imgPath = getExternalFilesDir(Environment.DIRECTORY_PICTURES)
 
         //insert hardcode data into db
 //        stepCountViewModel.insert(
@@ -50,10 +67,21 @@ class MainActivity : ComponentActivity() {
                         bottomBar = { BottomNavigation(navController = navController) }
                     ) {
 
-                        NavigationGraph(navController = navController, stepCountViewModel = stepCountViewModel)
+                        NavigationGraph(navController = navController, stepCountViewModel = stepCountViewModel, photoAndMapViewModel = photoAndMapViewModel, imgPath = imgPath, fusedLocationProviderClient = fusedLocationProviderClient)
                     }
                 }
             }
         }
+    }
+    @RequiresApi(Build.VERSION_CODES.M)
+    private fun hasPermissions(): Boolean {
+        if (checkSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            Log.d("aaaaaa", "No gps access")
+            requestPermissions(arrayOf(Manifest.permission.ACCESS_FINE_LOCATION), 1);
+            return true // assuming that the user grants permission
+        }else if (checkSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED){
+            Log.d("aaaaaa", "gps access")
+        }
+        return true
     }
 }
